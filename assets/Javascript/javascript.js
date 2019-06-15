57f00bbdc79a3b2cce1e44273f3252370e78e6d2
Sean Johnston <seanj310@gmail.com>
2019-06-15 13:03:24 -0400

    new branch

---
+++ assets/Javascript/javascript.js
@@ -0,0 +1,12 @@
+
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var userGuessArr = []
+var guessesRemaining = 15
+var computerGuess = letters[Math.floor[Math.random() = letters.length]]
+var wins = 0
+var losses = 0
+var userGuess = ""
+
+console.log(computerGuess)
+
